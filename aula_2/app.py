0aa4d2d4c51c704f5505a17f491f166c7e3728b2
Lucas Ricciardi de Salles <lucas.salles@4linux.com.br>
2019-08-07 22:15:11 -0300

    aula 3

--- aula_2/app.py
+++ aula_2/app.py
@@ -1,17 +1,32 @@
 
 import sys
 import os
+import time
 
 import flask
+import requests
 
+from routes.auth import blueprint as auth_blueprint
 
 app = flask.Flask(__name__)
 
+app.register_blueprint(auth_blueprint)
+
 @app.route('/')
 def index():
+
+    res = requests.get('https://gen-net.herokuapp.com/api/users/')
+    
+    users = res.json() if res.status_code == 200 else []
+
+    def extract(u):
+        return u
+
     context = {
-        'title': 'Python | Sysadmin'
+        'title': 'Python | Sysadmin',
+        'users': [ extract(u) for u in users ]
     }
+
     return flask.render_template('index.html', context=context)
 
 if __name__ == "__main__":
@@ -19,4 +34,4 @@ if __name__ == "__main__":
     root_module = os.path.abspath(os.path.curdir)
     sys.path.append(root_module)
 
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')  
