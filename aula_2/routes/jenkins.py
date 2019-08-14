66f07791ce5926fb371f7b7606ed5542a5424b23
Paulo Lopes <Paulo201098@gmail.com>
2019-08-13 22:24:19 -0300

    Jenkins

--- aula_2/routes/jenkins.py
+++ aula_2/routes/jenkins.py
@@ -1,19 +1,48 @@
-
 import flask
+import jenkins
 
 
 blueprint = flask.Blueprint('jenkins', __name__)
 
 @blueprint.route('/jenkins', methods=[ 'GET', 'POST' ])
-def jenkins():
-    context = {
-        'title': 'Python | Sysadmin'
-    }
-    return flask.render_template('jenkins.html', context=context)
+def get_jenkins():
+	
+	conn = jenkins.Jenkins('http://localhost:8081', 'admin', 'admin')
+	
+	jobs = [
+		conn.get_job_info(j['fullname']) for j in conn.get_jobs()
+	]
+	
+	context = {
+		'title': 'Python | Sysadmin',
+		'jobs': jobs
+	}
+
+	return flask.render_template('jenkins.html', context=context)
+
+@blueprint.route('/jenkins/build/<jobname>', methods=[ 'GET' ])
+def build_jenkins_job(jobname):
+
+	conn = jenkins.Jenkins('http://localhost:8081', 'admin', 'admin')
+	
+	conn.build_job(jobname)
+
+	return flask.redirect('/jenkins')
 
 @blueprint.route('/jenkins/<jobname>', methods=[ 'GET', 'POST' ])
 def jenkins_update(jobname):
-    context = {
-        'title': 'Python | Sysadmin'        
-    }
-    return flask.render_template('jenkins_update.html', context=context)
+
+	conn = jenkins.Jenkins('http://localhost:8081', 'admin', 'admin')
+
+	if flask.request.method == 'POST':
+		conn.reconfig_job(jobname, flask.request.form.get('xml').lstrip())
+
+	job = conn.get_job_info(jobname)
+	job['xml'] = conn.get_job_config(jobname)
+
+	context = {
+		'title': 'Python | Sysadmin',
+		'job': job         
+	}
+	
+	return flask.render_template('jenkins_update.html', context=context)
