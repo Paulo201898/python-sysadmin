25ab3d9ee2ef52c7b2cf156ec5347becdcf36ec7
Paulo Lopes <Paulo201098@gmail.com>
2019-08-15 21:56:39 -0300

    Commitando ldap3

--- aula_2/app.py
+++ aula_2/app.py
@@ -6,13 +6,15 @@ import time
 import flask
 import requests
 
+
 from routes.auth import blueprint as auth_blueprint
 from routes.jenkins import blueprint as jenkins_blueprint
 from routes.docker import blueprint as docker_blueprint
 from routes.gitlab import blueprint as gitlab_blueprint
 
-
 app = flask.Flask(__name__)
+
+app.secret_key = 'secret'
 
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(jenkins_blueprint)
@@ -22,23 +24,23 @@ app.register_blueprint(gitlab_blueprint)
 @app.route('/')
 def index():
 
-    res = requests.get('https://gen-net.herokuapp.com/api/users/')
-    
-    users = res.json() if res.status_code == 200 else []
+	res = requests.get('https://gen-net.herokuapp.com/api/users/')
+	
+	users = res.json() if res.status_code == 200 else []
 
-    def extract(u):
-        return u
+	def extract(u):
+		return u
 
-    context = {
-        'title': 'Python | Sysadmin',
-        'users': [ extract(u) for u in users ]
-    }
+	context = {
+		'title': 'Python | Sysadmin',
+		'users': [ extract(u) for u in users ]
+	}
 
-    return flask.render_template('index.html', context=context)
+	return flask.render_template('index.html', context=context)
 
 if __name__ == "__main__":
-    
-    root_module = os.path.abspath(os.path.curdir)
-    sys.path.append(root_module)
+	
+	root_module = os.path.abspath(os.path.curdir)
+	sys.path.append(root_module)
 
-    app.run(host='0.0.0.0')  
+	app.run(host='0.0.0.0')  
